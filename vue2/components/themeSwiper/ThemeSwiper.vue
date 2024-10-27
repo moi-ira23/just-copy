8a7328afa59ee3adb5a4efa4d20d5d3b64977aaa
moi-ira23 <irina.sorokina232325@gmail.com>
2024-10-27 22:22:05 +0300

    добавила центральное положение

--- vue2/components/themeSwiper/ThemeSwiper.vue
+++ vue2/components/themeSwiper/ThemeSwiper.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="switcher">
     <div class="switch" @click="() => toggleTheme()">
-      <div class="toggle" :class="{ 'toggle--toggled': isDarkMode }">
+      <div class="toggle" :class="toggleClass">
         <img
-          v-if="isDarkMode"
+          v-if="showMoon"
           class="toggle__image"
           src="https://img.icons8.com/tiny-color/32/bright-moon.png"
-          alt="луна"/>
+          alt="Луна"
+        />
         <img
-          v-else
+          v-if="showSun"
           class="toggle__image"
           src="https://img.icons8.com/ultraviolet/32/sun--v1.png"
-          alt="солнце"/>
+          alt="Солнце"
+        />
       </div>
     </div>
   </div>
@@ -21,22 +23,50 @@
 export default {
   name: "ThemeSwiper",
   created() {
-    const savedTheme = localStorage.getItem('theme');
+    const savedTheme = localStorage.getItem("theme");
     if (savedTheme) {
-      this.isDarkMode = savedTheme === 'dark';
+      this.isDarkMode = savedTheme === "dark";
+      this.position = savedTheme === "dark" ? "right" : "left";
+    } else {
+      this.position = "center";
     }
   },
   data() {
     return {
-      isDarkMode: false
+      isDarkMode: false,
+      position: "center",
     };
+  },
+  computed: {
+    toggleClass() {
+      return {
+        "toggle--left": this.position === "left",
+        "toggle--center": this.position === "center",
+        "toggle--right": this.position === "right",
+      };
+    },
+    showMoon() {
+      return this.position === "right";
+    },
+    showSun() {
+      return this.position === "left";
+    },
   },
   methods: {
     toggleTheme() {
-      this.isDarkMode = !this.isDarkMode;
-      localStorage.setItem('theme', this.isDarkMode ? 'dark' : 'light');
-    }
-  }
+      if (this.position === "center") {
+        this.position = "right";
+        this.isDarkMode = true;
+      } else if (this.position === "right") {
+        this.position = "left";
+        this.isDarkMode = false;
+      } else {
+        this.position = "right";
+        this.isDarkMode = true;
+      }
+      localStorage.setItem("theme", this.isDarkMode ? "dark" : "light");
+    },
+  },
 };
 </script>
 
